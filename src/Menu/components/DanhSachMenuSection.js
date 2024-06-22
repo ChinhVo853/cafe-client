26c92b87654e465921f6e5cf2047df41c4ce2fc8
LGBK23 <0306211114@caothang.edu.vn>
2024-06-22 22:07:20 +0700

    menuluot

--- src/Menu/components/DanhSachMenuSection.js
+++ src/Menu/components/DanhSachMenuSection.js
@@ -1,45 +1,50 @@
 const DanhSachMenuSection = ({ menuIsOpen, handleMenuToggle }) => {
+  const handleMenuItemClick = (event) => {
+    event.preventDefault();
+    const targetId = event.target.getAttribute("href").substring(1); // Lấy ID của phần tử đích (bỏ qua ký tự #)
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <>
       <div className={`menu ${menuIsOpen ? "open" : ""}`} id="menu">
-      <ul>
-    <li>
-      <a href="#milk-tea">
-        <i className="fas fa-coffee"></i> Trà sữa
-      </a>
-    </li>
-    <li>
-      <a href="#fruit-tea">
-        <i className="fas fa-apple-alt"></i> Trà trái cây
-      </a>
-    </li>
-    <li>
-      <a href="#soda">
-        <i className="fas fa-wine-glass-alt"></i> Soda
-      </a>
-    </li>
-    <li>
-      <a href="#coffee">
-        <i className="fas fa-mug-hot"></i> Cà phê hạt
-      </a>
-    </li>
-    <li>
-      <a href="#ice-blend">
-        <i className="fas fa-ice-cream"></i> Đá xay
-      </a>
-    </li>
-    <li>
-      <a href="#other">
-        <i className="fas fa-utensils"></i> Món khác
-      </a>
-    </li>
-  </ul>
+        <ul>
+          <li>
+            <a href="#milk-tea" onClick={handleMenuItemClick}>
+              <i className="fas fa-coffee"></i> Trà sữa
+            </a>
+          </li>
+          <li>
+            <a href="#fruit-tea" onClick={handleMenuItemClick}>
+              <i className="fas fa-apple-alt"></i> Trà trái cây
+            </a>
+          </li>
+          <li>
+            <a href="#soda" onClick={handleMenuItemClick}>
+              <i className="fas fa-wine-glass-alt"></i> Soda
+            </a>
+          </li>
+          <li>
+            <a href="#coffee" onClick={handleMenuItemClick}>
+              <i className="fas fa-mug-hot"></i> Cà phê hạt
+            </a>
+          </li>
+          <li>
+            <a href="#ice-blend" onClick={handleMenuItemClick}>
+              <i className="fas fa-ice-cream"></i> Đá xay
+            </a>
+          </li>
+          <li>
+            <a href="#other" onClick={handleMenuItemClick}>
+              <i className="fas fa-utensils"></i> Món khác
+            </a>
+          </li>
+        </ul>
       </div>
-      <button
-        className="menu-toggle"
-        id="menu-toggle"
-        onClick={handleMenuToggle}
-      >
+      <button className="menu-toggle" id="menu-toggle" onClick={handleMenuToggle}>
         ☰
       </button>
     </>
