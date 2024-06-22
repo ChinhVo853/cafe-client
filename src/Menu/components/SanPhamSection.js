75e988e9015b3e98e1d7c73ba5a7cff5eee3f177
LGBK23 <0306211114@caothang.edu.vn>
2024-06-23 02:17:23 +0700

    homemenu

--- src/Menu/components/SanPhamSection.js
+++ src/Menu/components/SanPhamSection.js
@@ -5,7 +5,8 @@ const SanPhamSection = ({
   handleAddToCart,
   handleCloseButtonClick,
   productOptions,
-  data,
+  productId,
+  name,
   price,
 }) => {
   return (
@@ -13,20 +14,19 @@ const SanPhamSection = ({
       <div className="product-card">
         <img
           src="https://via.placeholder.com/200"
-          alt={`Sản phẩm ${data.ten_mon}`}
+          alt={`Sản phẩm ${productId}`}
           className="product-image"
         />
         <div className="product-info">
-          <div className="product-name">{data.ten_mon}</div>
+          <div className="product-name">{name}</div>
           <div className="product-price">{price.toLocaleString()}đ</div>
           <button
             className="add-button"
-            onClick={() => handleAddButtonClick(data.ten_mon)}
+            onClick={() => handleAddButtonClick(productId)}
           >
             +
           </button>
           <div
-
             className={`product-options ${productOptions[productId] ? "open" : ""
               }`}
           >
@@ -43,13 +43,11 @@ const SanPhamSection = ({
               <option value="S">S</option>
               <option value="M">M</option>
               <option value="L">L</option>
-
             </select>
-
-            <label htmlFor={`quantity-${data.ten_mon}`}>Số lượng:</label>
+            <label htmlFor={`quantity-${productId}`}>Số lượng:</label>
             <input
               type="number"
-              id={`quantity-${data.ten_mon}`}
+              id={`quantity-${productId}`}
               defaultValue="1"
               min="1"
             />
@@ -57,13 +55,13 @@ const SanPhamSection = ({
               className="add-to-cart-button"
               onClick={() =>
                 handleAddToCart(
-                  data.ten_mon,
-                  document.getElementById(`size-${data.ten_mon}`).value,
+                  productId,
+                  document.getElementById(`size-${productId}`).value,
                   parseInt(
-                    document.getElementById(`quantity-${data.ten_mon}`).value
+                    document.getElementById(`quantity-${productId}`).value
                   ),
                   price,
-                  data.ten_mon
+                  name
                 )
               }
             >
