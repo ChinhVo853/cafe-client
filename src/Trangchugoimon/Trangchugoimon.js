3efae172428b874c6f40b6f671e2f3c2452fc655
ChinhVo853 <chinhvo853@gmail.com>
2024-06-29 20:43:45 +0700

    yeuCau

--- src/Trangchugoimon/Trangchugoimon.js
+++ src/Trangchugoimon/Trangchugoimon.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import Phanhoi from "./components/Phanhoi";
 import { useParams } from "react-router-dom";
+import { TaoYeuCau } from "./API/Api"; // Ensure this import is correct
 
 function Trangchugoimon() {
   const { ban } = useParams();
+  const [yeuCau, setYeuCau] = useState(null);
+  const [noiDung, setNoiDung] = useState();
+
   const [showCallStaffForm, setShowCallStaffForm] = useState(false);
   const [requestItems, setRequestItems] = useState({
     tương_ớt: false,
@@ -11,9 +15,8 @@ function Trangchugoimon() {
     giấy: false,
     tăm: false,
     nước_tương: false,
-    // Thêm các mục yêu cầu khác ở đây
   });
-  console.log(localStorage.getItem("QR"));
+
   const toggleCallStaffForm = () => {
     setShowCallStaffForm(!showCallStaffForm);
   };
@@ -22,27 +25,16 @@ function Trangchugoimon() {
     setShowCallStaffForm(true);
   };
 
-  const handleCheckboxChange = (itemName) => {
-    setRequestItems((prevItems) => ({
-      ...prevItems,
-      [itemName]: !prevItems[itemName],
-    }));
-  };
-
   const handleSubmitForm = () => {
-    // Xử lý khi submit form (ở đây có thể gửi yêu cầu lên server hoặc xử lý logic khác)
     console.log("Submitted request items:", requestItems);
     setShowCallStaffForm(false);
-    // Thêm các logic xử lý khác tại đây nếu cần
   };
-  /**********************************THANHTOAN*************************************** */
+
   const [showGoiThanhToan, setShowGoiThanhToan] = useState(false);
   const [requestItemsthanhtoan, setRequestItemsthanhtoan] = useState({
     tien_mat: false,
     chuyen_khoang: false,
     the: false,
-
-    // Thêm các mục yêu cầu khác ở đây
   });
 
   const toggleGoiThanhToan = () => {
@@ -53,20 +45,49 @@ function Trangchugoimon() {
     setShowGoiThanhToan(true);
   };
 
-  const handleCheckboxChangethanhtoan = (itemName) => {
-    setRequestItemsthanhtoan((prevItems) => ({
-      ...prevItems,
-      [itemName]: !prevItems[itemName],
-    }));
+  const YeuCau = (value) => {
+    setYeuCau(value);
   };
 
-  const handleSubmitFormthanhtoan = () => {
-    // Xử lý khi submit form (ở đây có thể gửi yêu cầu lên server hoặc xử lý logic khác)
-    console.log("Submitted request items:", requestItems);
-    setShowGoiThanhToan(false);
-    // Thêm các logic xử lý khác tại đây nếu cần
+  const YeucauGoiNhanVien = async () => {
+    const data = {
+      yeuCau: "Gọi nhân viên",
+      noiDung: "Gọi nhân viên: " + noiDung,
+      ban: ban,
+    };
+
+    const result = await TaoYeuCau(data);
+    console.log(result);
   };
-  /***************************************************************************************/
+
+  const YeuCauThanhToan = async () => {
+    const data = {};
+    if (yeuCau === 1) {
+      data = {
+        yeuCau: "Thanh toán",
+        noiDung: "Gọi thanh toán: thanh toán bằng tiền mặt",
+        ban: ban,
+      };
+    } else if (yeuCau === 2) {
+      data = {
+        yeuCau: "Thanh toán",
+        noiDung: "Gọi thanh toán: thanh toán chuyển khoản",
+        ban: ban,
+      };
+    } else {
+      data = {
+        yeuCau: "Thanh toán",
+        noiDung: "Gọi thanh toán: thanh toán bằng thẻ",
+        ban: ban,
+      };
+    }
+
+    const result = await TaoYeuCau(data);
+  };
+  console.log(noiDung);
+  const handleNameChange = (e) => {
+    setNoiDung(e.target.value);
+  };
   return (
     <>
       <div
@@ -74,7 +95,6 @@ function Trangchugoimon() {
           showCallStaffForm ? "blur-background" : ""
         }`}
       >
-        {/* <!-- Navbar & Hero Start --> */}
         <div className="container-xxl position-relative p-0">
           <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
             <a href="" className="navbar-brand p-0">
@@ -82,28 +102,8 @@ function Trangchugoimon() {
                 {" "}
                 <i className="fa fa-utensils me-3"></i>CHAOBỈNH
               </h1>
-              {/* <!-- <img src="img/logo.png" alt="Logo"> --> */}
             </a>
-            {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-                        <span className="fa fa-bars"></span>
-                    </button> */}
-            <div className="collapse navbar-collapse" id="navbarCollapse">
-              {/* <div className="navbar-nav ms-auto py-0 pe-4">
-                        <a href="index.html" className="nav-item nav-link active">Home</a>
-                        <a href="about.html" className="nav-item nav-link">About</a>
-                        <a href="service.html" className="nav-item nav-link">Service</a>
-                        <a href="menu.html" className="nav-item nav-link">Menu</a>
-                        <div className="nav-item dropdown">
-                            <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-                            <div className="dropdown-menu m-0">
-                                <a href="booking.html" className="dropdown-item">Booking</a>
-                                <a href="team.html" className="dropdown-item">Our Team</a>
-                                <a href="testimonial.html" className="dropdown-item">Testimonial</a>
-                            </div>
-                        </div>
-                        <a href="contact.html" className="nav-item nav-link">Contact</a>
-                    </div> */}
-            </div>
+            <div className="collapse navbar-collapse" id="navbarCollapse"></div>
           </nav>
 
           <div className="container-xxl py-5 bg-dark hero-header mb-5">
@@ -118,20 +118,18 @@ function Trangchugoimon() {
                 </div>
                 <div className="col-lg-6 text-center text-lg-end overflow-hidden">
                   <div className="xoayxoay"></div>
-                  {/* <img className="img-fluid" src="src/img/hero.png" alt=""/> */}
                 </div>
               </div>
             </div>
           </div>
         </div>
-        {/* <!-- Navbar & Hero End --> */}
         <div className="welcomeabouus col-lg-6">
           <h1 className="mb-4">
             Welcome to <i className="fa fa-utensils text-primary me-2"></i>
             CHAOBỈNH
           </h1>
         </div>
-        {/* <!-- Service Start --> */}
+
         <div className="container-xxl py-5">
           <div className="container">
             <div className="iconchucnangcha row g-4">
@@ -180,81 +178,37 @@ function Trangchugoimon() {
                   </div>
                 </div>
               </div>
-              {/* <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
-                        <div className="service-item rounded pt-3">
-                            <div className="p-4">
-                                <i className="iconchucnang fa fa-3x fa-headset text-primary mb-4"></i>
-                                <h5>24/7 Service</h5>
-                            </div>
-                        </div>
-                    </div> */}
             </div>
           </div>
         </div>
-        {/* <!-- Service End --> */}
 
-        {/* <!-- About Start --> */}
         <div className="container-xxl py-5">
           <div className="container">
             <div className="row g-5 align-items-center"></div>
           </div>
         </div>
-        {/* <!-- About End --> */}
 
-        <a href="/Phanhoi" className="fixed-button">
+        <a href={`/Phanhoi/${ban}`} className="fixed-button">
           <i className="fa fa-bullhorn"></i>
         </a>
       </div>
-      {/* GỌI NHÂN VIÊN */}
+
       {showCallStaffForm && (
         <div className="call-staff-form">
           <div className="form-content">
             <h2>Gọi Nhân Viên</h2>
-            <textarea placeholder="Nhập yêu cầu của bạn..." rows="4"></textarea>
+            <textarea
+              placeholder="Nhập yêu cầu của bạn..."
+              rows="4"
+              onChange={handleNameChange}
+            ></textarea>
             <div className="request-items">
-              {/* <label>
-                <input
-                  type="checkbox"
-                  checked={requestItems.tương_ớt}
-                  onChange={() => handleCheckboxChange('tương_ớt')}
-                />
-                Tương ớt
-              </label>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={requestItems.tương_cà}
-                  onChange={() => handleCheckboxChange('tương_cà')}
-                />
-                Tương cà
-              </label>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={requestItems.giấy}
-                  onChange={() => handleCheckboxChange('giấy')}
-                />
-                Giấy
-              </label>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={requestItems.tăm}
-                  onChange={() => handleCheckboxChange('tăm')}
-                />
-                Tăm
-              </label>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={requestItems.nước_tương}
-                  onChange={() => handleCheckboxChange('nước_tương')}
-                />
-                Nước tương
-              </label> */}
-              {/* Thêm các mục yêu cầu khác ở đây */}
+              {/* Add your checkbox items here */}
             </div>
-            <button className="btn btn-primary mt-3" onClick={handleSubmitForm}>
+            <button
+              className="btn btn-primary mt-3"
+              onClick={YeucauGoiNhanVien}
+            >
               <i className="fa fa-paper-plane"></i> Gửi Yêu Cầu
             </button>
             <button
@@ -267,44 +221,55 @@ function Trangchugoimon() {
         </div>
       )}
 
-      {/* GỌI THANH TOÁN */}
       {showGoiThanhToan && (
         <div className="call-staff-form">
           <div className="form-content">
             <h2>CHỌN CÁCH THANH TOÁN</h2>
-
-            <div className="request-items">
-              <label>
+            <div className="request-items" style={{ display: "flex" }}>
+              <div className="form-check">
                 <input
-                  type="checkbox"
-                  checked={requestItemsthanhtoan.tien_mat}
-                  onChange={() => handleCheckboxChangethanhtoan("tien_mat")}
+                  className="form-check-input"
+                  type="radio"
+                  value={1}
+                  checked={yeuCau === 1}
+                  onChange={(e) => YeuCau(Number(e.target.value))}
+                  name="flexRadioDefault"
+                  id="flexRadioDefault1"
                 />
-                Tiền mặt
-              </label>
-              <label>
+                <label className="form-check-label" htmlFor="flexRadioDefault1">
+                  Tiền mặt
+                </label>
+              </div>
+              <div className="form-check">
                 <input
-                  type="checkbox"
-                  checked={requestItemsthanhtoan.chuyen_khoang}
-                  onChange={() =>
-                    handleCheckboxChangethanhtoan("chuyen_khoang")
-                  }
+                  className="form-check-input"
+                  type="radio"
+                  value={2}
+                  checked={yeuCau === 2}
+                  onChange={(e) => YeuCau(Number(e.target.value))}
+                  name="flexRadioDefault"
+                  id="flexRadioDefault2"
                 />
-                Chuyển khoảng
-              </label>
-              <label>
+                <label className="form-check-label" htmlFor="flexRadioDefault2">
+                  Chuyển khoản
+                </label>
+              </div>
+              <div className="form-check">
                 <input
-                  type="checkbox"
-                  checked={requestItemsthanhtoan.the}
-                  onChange={() => handleCheckboxChangethanhtoan("the")}
+                  className="form-check-input"
+                  type="radio"
+                  value={3}
+                  checked={yeuCau === 3}
+                  onChange={(e) => YeuCau(Number(e.target.value))}
+                  name="flexRadioDefault"
+                  id="flexRadioDefault3"
                 />
-                Thẻ
-              </label>
+                <label className="form-check-label" htmlFor="flexRadioDefault3">
+                  Thẻ
+                </label>
+              </div>
             </div>
-            <button
-              className="btn btn-primary mt-3"
-              onClick={handleSubmitFormthanhtoan}
-            >
+            <button className="btn btn-primary mt-3" onClick={YeuCauThanhToan}>
               <i className="fa fa-paper-plane"></i> Gửi Yêu Cầu
             </button>
             <button
--- src/Trangchugoimon/components/Phanhoi.js
+++ src/Trangchugoimon/components/Phanhoi.js
@@ -1,40 +1,78 @@
-import React from "react";
-
+import React, { useEffect, useState, useCallback } from "react";
+import { LayYeuCau } from "../API/Api";
+import { useParams } from "react-router-dom";
 const Phanhoi = () => {
-    const handleGoBack = () => {
-        window.history.back();
-    };
+  const { ban } = useParams();
+  const [data, setData] = useState();
 
-    const handleViewOrders = () => {
-        // Xử lý khi người dùng bấm vào xem các món đã đặt
-        console.log("View orders clicked");
-        // Thực hiện các hành động khác nếu cần thiết
-    };
+  useEffect(() => {
+    LayData();
+  }, [ban]);
 
-    return (
+  const LayData = useCallback(async () => {
+    try {
+      const result = await LayYeuCau(ban);
+      setData(result.data);
+    } catch (error) {
+      console.error("Failed to fetch data", error);
+    }
+  }, []);
 
+  const handleGoBack = () => {
+    window.history.back();
+  };
+
+  const handleViewOrders = () => {
+    // Xử lý khi người dùng bấm vào xem các món đã đặt
+    console.log("View orders clicked");
+    // Thực hiện các hành động khác nếu cần thiết
+  };
+
+  return (
+    <>
+      {data && (
         <div className="feedback-page">
-            <button className="home-button" onClick={handleGoBack}>
-                <i className="fas fa-home"></i>
-            </button>
-            <div className="feedback-content">
-                <h3>Phản hồi từ nhà hàng</h3>
-                <a className="form-datmon" href="/Cacmondadat" onClick={handleViewOrders}>
-                    Các món đã đặt...
-                </a>
-                {/* Nội dung phản hồi và các phần khác có thể được thêm vào đây */}
-            </div>
-            <div className="bodyphanhoi">
-            <div className="message-box">
-          <div className="message">
-            <p>Yêu cầu của bạn đã được gửi.</p>
+          <button className="home-button" onClick={handleGoBack}>
+            <i className="fas fa-home"></i>
+          </button>
+          <div className="feedback-content">
+            <h3>Phản hồi từ nhà hàng</h3>
+            <a
+              className="form-datmon"
+              href="/Cacmondadat"
+              onClick={handleViewOrders}
+            >
+              Các món đã đặt...
+            </a>
+            {/* Nội dung phản hồi và các phần khác có thể được thêm vào đây */}
+          </div>
+          <div className="bodyphanhoi">
+            {data.map((item, key) => (
+              <div key={key}>
+                <div className="message-box">
+                  <div
+                    className="message"
+                    style={{ backgroundColor: "antiquewhite" }}
+                  >
+                    <p>{item.noi_dung}</p>
+                  </div>
+                </div>
+                {item.trang_thai == 1 ? (
+                  <div className="message-box">
+                    <div className="message">
+                      <p>Chúng tôi đã nhận được yêu cầu của bạn</p>
+                    </div>
+                  </div>
+                ) : (
+                  <div></div>
+                )}
+              </div>
+            ))}
           </div>
         </div>
-            </div>
-
-        </div>
-
-    );
+      )}
+    </>
+  );
 };
 
 export default Phanhoi;
